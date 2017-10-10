a1d1464fe250a13326764209c3378ba03a386d51
barneynicholls <barney.nicholls@bluetoad.net>
2017-10-10 13:53:57 +0100

    Added additional config tests

--- sample.config.py
+++ sample.config.py
@@ -1,12 +1,20 @@
 class TwitterConfig:
-    API_KEY = 'twitter api key'
-    SECRET = 'twitter secret'
+    CONSUMER_KEY = 'your_twitter_consumer_key'
+    CONSUMER_SECRET = 'your_twitter_consumer_secret'
+    ACCESS_TOKEN = 'your_twitter_access_token'
+    ACCESS_TOKEN_SECRET = 'your_twitter_access_token_secret'
 
 
 class RaspberryPiConfig:
-    WIND_SPEED_GPIO = 0
+    # I2C Interface bus. Rev 1 Pi uses 0, Rev 2 Pi uses 1
+    I2C_INTERFACE_BUS = -1
+    # gpio pin wind speed anenometer is connected to
+    WIND_SPEED_GPIO = -1
 
+class ThingSpeakConfig:
+    KEY = 'your_thingspeak_key_for_the_channel_you_upload_to'
 
 class Config:
     Twitter = TwitterConfig()
     RaspberryPi = RaspberryPiConfig()
+    ThingSpeak = ThingSpeakConfig()
--- test/config.tests.py
+++ test/config.tests.py
@@ -1,11 +1,32 @@
 import unittest
 
 
-class RaspberryPiTest(unittest.TestCase):
-    def testWindSpeedGpioPinSet(self):
+class ConfigurationTests(unittest.TestCase):
+    def testThingSpeakSettings(self):
         import config
         cfg = config.Config()
-        self.assertGreater(cfg.RaspberryPi.WIND_SPEED_GPIO, 0, 'Wind Speed GPIO Pin Not Set')
+        self.assertNotEqual(cfg.ThingSpeak.KEY, 'your_thingspeak_key_for_the_channel_you_upload_to',
+                            'thingspeak channel key not set')
+
+    def testRaspberryPiSettings(self):
+        import config
+        cfg = config.Config()
+        self.assertGreater(cfg.RaspberryPi.WIND_SPEED_GPIO, -1,
+                           'wind speed gpio pin not set')
+        self.assertGreater(cfg.RaspberryPi.I2C_INTERFACE_BUS, -1,
+                           'i2c interface bus not set')
+
+    def testTwitterSettings(self):
+        import config
+        cfg = config.Config()
+        self.assertNotEqual(cfg.Twitter.ACCESS_TOKEN, 'your_twitter_access_token',
+                            'twitter access token not set')
+        self.assertNotEqual(cfg.Twitter.ACCESS_TOKEN_SECRET, 'your_twitter_access_token_secret',
+                            'twitter access token secret not set')
+        self.assertNotEqual(cfg.Twitter.CONSUMER_KEY, 'your_twitter_consumer_key',
+                            'twitter consumer key not set')
+        self.assertNotEqual(cfg.Twitter.CONSUMER_SECRET, 'your_twitter_consumer_secret',
+                            'twitter consumer secret not set')
 
 
 if __name__ == '__main__':
