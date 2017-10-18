462959ae525d322fbc574f8b623b011c89ab81cb
barneynicholls <barney.nicholls@bluetoad.net>
2017-10-18 13:05:28 +0100

    update test to write file to desktop

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-rom os.path import join
+from os.path import join
+
 def write():
-        print('Creating a new file')
-        path = "home/pi"
-        name = raw_input('Enter a name for your file: ')+'.txt'  # Name of text file coerced with +.txt
+    print('Creating a new file')
+    path = "~/home/pi/Desktop.txt"
 
-        try:
-            file = open(join(path, name),'w')   # Trying to create a new file or open one
-            file.close()
+    try:
+        file = open(join(path),'w')   # Trying to create a new file or open one
+        file.close()
 
-        except:
-            print('Something went wrong! Cannot tell what?')
-            sys.exit(0) # quit Python
+    except:
+        print('Something went wrong! Cannot tell what?')
+
+        
+if __name__ == "__main__":
+    write()
